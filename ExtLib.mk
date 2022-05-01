98b63cce5a6917e225685c8eda18bdba233f87d6
rankaisija64 <65511061+rankaisija64@users.noreply.github.com>
2022-05-01 11:15:11 +0300

    Store .o files locally

--- ExtLib.mk
+++ ExtLib.mk
@@ -8,8 +8,8 @@ XM_C      = libxm/src/context.c \
 ZIP_C     = zip/src/zip.c
 MINAUD_C  = miniaudio.c
 
-ExtLib_Linux_O  = $(foreach f,$(ExtLib_C:.c=.o) $(XM_C:.c=.o) $(MINAUD_C:.c=.o) $(ZIP_C:.c=.o), $(C_INCLUDE_PATH)/bin/linux/$f)
-ExtLib_Win32_O  = $(foreach f,$(ExtLib_C:.c=.o) $(XM_C:.c=.o) $(MINAUD_C:.c=.o) $(ZIP_C:.c=.o), $(C_INCLUDE_PATH)/bin/win32/$f)
+ExtLib_Linux_O  = $(foreach f,$(ExtLib_C:.c=.o) $(XM_C:.c=.o) $(MINAUD_C:.c=.o) $(ZIP_C:.c=.o), bin/linux/$f)
+ExtLib_Win32_O  = $(foreach f,$(ExtLib_C:.c=.o) $(XM_C:.c=.o) $(MINAUD_C:.c=.o) $(ZIP_C:.c=.o), bin/win32/$f)
 
 # Make build directories
 $(shell mkdir -p bin/ $(foreach dir, \
@@ -19,15 +19,13 @@ $(shell mkdir -p bin/ $(foreach dir, \
 	
 extlib_linux: $(ExtLib_Linux_O)
 extlib_win32: $(ExtLib_Win32_O)
-extlib_clean:
-	@rm -f -R  $(C_INCLUDE_PATH)/bin
 	
-$(C_INCLUDE_PATH)/bin/win32/ExtLib.o: $(C_INCLUDE_PATH)/ExtLib.c $(C_INCLUDE_PATH)/ExtLib.h
-$(C_INCLUDE_PATH)/bin/win32/%.o: $(C_INCLUDE_PATH)/%.c
+bin/win32/ExtLib.o: $(C_INCLUDE_PATH)/ExtLib.c $(C_INCLUDE_PATH)/ExtLib.h
+bin/win32/%.o: $(C_INCLUDE_PATH)/%.c
 	@echo "$(PRNT_RSET)[$(PRNT_BLUE)$(notdir $@)$(PRNT_RSET)]"
 	@i686-w64-mingw32.static-gcc -c -o $@ $< $(OPT_WIN32) $(CFLAGS) -Wno-format-truncation -Wno-strict-aliasing -Wno-implicit-function-declaration -DNDEBUG -D_WIN32
 	
-$(C_INCLUDE_PATH)/bin/linux/ExtLib.o: $(C_INCLUDE_PATH)/ExtLib.c $(C_INCLUDE_PATH)/ExtLib.h
-$(C_INCLUDE_PATH)/bin/linux/%.o: $(C_INCLUDE_PATH)/%.c
+bin/linux/ExtLib.o: $(C_INCLUDE_PATH)/ExtLib.c $(C_INCLUDE_PATH)/ExtLib.h
+bin/linux/%.o: $(C_INCLUDE_PATH)/%.c
 	@echo "$(PRNT_RSET)[$(PRNT_BLUE)$(notdir $@)$(PRNT_RSET)]"
 	@gcc -c -o $@ $< $(OPT_LINUX) $(CFLAGS) -Wno-format-truncation -Wno-strict-aliasing -Wno-implicit-function-declaration -DNDEBUG
